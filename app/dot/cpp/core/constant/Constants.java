a3365dd3fc636320ec52ec4cbd34b4dc526a51d6
Codrin Sirboiu <codrin.sirboiu@gmail.com>
2022-07-22 11:58:57 +0300

    the core is real now

---
+++ app/dot/cpp/core/constant/Constants.java
@@ -0,0 +1,13 @@
+package dot.cpp.core.constant;
+
+public class Constants {
+  public static final int EMAIL_FIELD_LENGTH = 140;
+
+  public static final int COMMON_FIELD_LENGTH = 70;
+
+  public static final int NAME_FIELD_LENGTH = 120;
+
+  public static final int LONG_FIELD_LENGTH = 200;
+
+  public static final int DESCRIPTION_FIELD_LENGTH = 2000;
+}
--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 name := "core-component"
 organization := "dot.cpp"
 
-version := "1.0-SNAPSHOT"
+version := "1.0"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
