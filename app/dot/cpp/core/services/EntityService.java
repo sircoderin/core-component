1b7d6ae747401280a2600542174ccd428a7a28b3
Andrew Tookay <andrewtookay@gmail.com>
2022-08-06 17:26:59 +0300

    add count and pagination methods

--- app/dot/cpp/core/services/EntityService.java
+++ app/dot/cpp/core/services/EntityService.java
@@ -1,5 +1,6 @@
 package dot.cpp.core.services;
 
+import com.typesafe.config.Config;
 import dot.cpp.repository.models.BaseEntity;
 import dev.morphia.query.experimental.filters.Filter;
 import dot.cpp.repository.repository.BaseRepository;
@@ -7,9 +8,11 @@ import java.util.List;
 
 public class EntityService<T extends BaseEntity> {
   private final BaseRepository<T> repository;
+  private final int pageSize;
 
-  public EntityService(BaseRepository<T> repository) {
+  public EntityService(BaseRepository<T> repository, Config config) {
     this.repository = repository;
+    this.pageSize = config.getInt("list.page.size");
   }
 
   public T findById(String id) {
@@ -32,12 +35,34 @@ public class EntityService<T extends BaseEntity> {
     return filter == null ? repository.listAll() : repository.listWithFilter(filter);
   }
 
-  public List<T> listAllPaginated(int pageSize, int pageNum) {
-    return repository.listAllPaginated(pageSize, pageNum);
+  public List<T> listAllPaginated(int pageNum) {
+    return repository.listAllPaginated(pageSize, pageNum - 1);
   }
 
-  public List<T> listWithFilterPaginated(Filter filter, int pageSize, int pageNum) {
-    return filter == null ? repository.listAll() : repository.listWithFilterPaginated(filter, pageSize, pageNum);
+  public List<T> listWithFilterPaginated(Filter filter, int pageNum) {
+    return filter == null ? repository.listAllPaginated(pageSize, pageNum - 1) : repository.listWithFilterPaginated(filter, pageSize, pageNum - 1);
+  }
+
+  public long count() {
+    return repository.count();
+  }
+
+  public long count(Filter filter) {
+    return filter == null ? repository.count() : repository.count(filter);
+  }
+
+  public int getNumberPages() {
+    final var numEntities = count();
+    return (int) getNumberPages(numEntities);
+  }
+
+  public int getNumberPages(Filter filter) {
+    final var numEntities = count(filter);
+    return (int) getNumberPages(numEntities);
+  }
+
+  private long getNumberPages(long numEntities) {
+    return numEntities % pageSize == 0 ? numEntities / pageSize : numEntities / pageSize + 1;
   }
 
   public void save(T entity) {
