f2929008b029116d1a6cab2827d5799a79afc5bb
sircodrin <codrin.sirboiu@gmail.com>
2023-06-01 23:49:09 +0300

    temporary init

--- app/dot/cpp/core/services/UserService.java
+++ app/dot/cpp/core/services/UserService.java
@@ -45,8 +45,8 @@ public class UserService extends EntityService<User, InviteUserRequest> {
     user.setPassword("temporary");
     user.setStatus(UserStatus.INACTIVE);
     user.setResetPasswordUuid(resetPasswordUuid);
-    user.setFullName("Mihai Popescu");
-    user.setDocumentId("AZ 123456");
+    user.setFullName("temporary");
+    user.setDocumentId("temporary");
 
     save(user);
 
